36c4d6fa6d377c3778b3bffede876e9c1148d211
Vasyl Vavrychuk <vvavrychuk@gmail.com>
2017-03-24 16:00:13 +0200

    added web UI

--- service/src/main/java/uni/service/UniServiceApplication.java
+++ service/src/main/java/uni/service/UniServiceApplication.java
@@ -3,7 +3,12 @@ package uni.service;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import uni.data.NewsDAO;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class UniServiceApplication extends Application<UniServiceConfiguration> {
     public static void main(String[] args) throws Exception {
@@ -22,6 +27,18 @@ public class UniServiceApplication extends Application<UniServiceConfiguration> 
     @Override
     public void run(UniServiceConfiguration configuration,
                     Environment environment) {
+        // Enable CORS headers
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
         environment.jersey().register(new NewsResource(new NewsDAO(configuration.getJdbcConnectionUrl())));
     }
 }
